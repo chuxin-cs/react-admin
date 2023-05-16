7cf92e37a58896978417632af5e94daf19636b14
chuxin <15115359055@139.com>
2023-05-16 13:32:45 +0800

    feat:  hooks 入口

---
+++ src/hooks/index.ts
@@ -0,0 +1,11 @@
+import useMount from "./useMount";
+import useEffectOnce from "./useEffectOnce";
+import useToggle from "./useToggle";
+import useBoolean from "./useBoolean";
+
+export {
+    useMount,
+    useEffectOnce,
+    useBoolean,
+    useToggle
+}
