16f8f169055fd32af8f781cf603a6819dfee29de
chuxin <15115359055@139.com>
2023-05-17 11:31:18 +0800

    feat:  main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-
-// antd
 import {ConfigProvider} from 'antd';
 import zhCN from 'antd/locale/zh_CN';
-import 'antd/dist/reset.css';
+import {RouterProvider} from 'react-router-dom';
 
+// css 样式
+import "./styles/index.less";
+import 'antd/dist/reset.css';
 
 // 路由
 import router from './router';
-import {RouterProvider} from 'react-router-dom'
-
-
-// index.css
-import "./styles/index.css"
-
 
 // 挂载
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
