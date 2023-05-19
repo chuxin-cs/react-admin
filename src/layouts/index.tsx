54306a75a1ae11ecee77383b20dc22e34e763091
chuxin <15115359055@139.com>
2023-05-19 11:30:56 +0800

    feat:  完成react-router-dom改造~

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,7 @@
 import {Outlet} from "react-router-dom";
 
 const LayoutIndex = (props: any) => {
-    console.log(props, '---')
+    console.log(props, 'https://github.com/HalseySpicy/Hooks-Admin/blob/master/src/layouts/index.tsx')
     return (
         <div>
             <div>111</div>
