4d1be4e2b0715af748aa83b34f1dc82f10e72b81
chuxin <15115359055@139.com>
2023-05-22 19:27:24 +0800

    feat: 去除不要的代码

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {ConfigProvider} from 'antd';
-import zhCN from 'antd/locale/zh_CN';
-import {RouterProvider} from 'react-router-dom';
-import router from './router';
 
 // css 样式
 import "./styles/index.less";
